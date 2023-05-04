03901a6fbd8705a90a718485f062cba57661930e
Vakoi <119035856+Vakoi@users.noreply.github.com>
2023-05-04 08:25:04 +0300

    Add files via upload

---
+++ День 12/1/1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(640, 101);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(115, 23);
+            this.btnFilter.TabIndex = 0;
+            this.btnFilter.Text = "Фильтр";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(640, 206);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(115, 23);
+            this.btnSort.TabIndex = 1;
+            this.btnSort.Text = "Сортировка";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 12);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(570, 229);
+            this.txtResult.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnFilter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFilter;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ День 12/1/1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Email { get; set; }
+            public string[] Interests { get; set; }
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person { Name = "John", Age = 25, Email = "john@example.com", Interests = new string[] { "Coding", "Gaming", "Music" } },
+                new Person { Name = "Emily", Age = 30, Email = "emily@example.com", Interests = new string[] { "Hiking", "Photography", "Cooking" } },
+                new Person { Name = "Michael", Age = 20, Email = "michael@example.com", Interests = new string[] { "Sports", "Traveling", "Reading" } }
+            };
+            var coders = people.Where(p => p.Interests.Contains("Coding"));
+            var result = string.Join(Environment.NewLine, coders.Select(p => $"{p.Name} ({p.Age})"));
+
+            txtResult.Text = result;
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person { Name = "John", Age = 25, Email = "john@example.com", Interests = new string[] { "Coding", "Gaming", "Music" } },
+                new Person { Name = "Emily", Age = 30, Email = "emily@example.com", Interests = new string[] { "Hiking", "Photography", "Cooking" } },
+                new Person { Name = "Michael", Age = 20, Email = "michael@example.com", Interests = new string[] { "Sports", "Traveling", "Reading" } }
+            };
+
+            var sortedPeople = people.OrderBy(p => p.Name);
+            var result = string.Join(Environment.NewLine, sortedPeople.Select(p => $"{p.Name} ({p.Age})"));
+
+            txtResult.Text = result;
+        }
+    }
+}
